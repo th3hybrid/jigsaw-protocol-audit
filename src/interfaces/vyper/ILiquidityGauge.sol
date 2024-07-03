eb5959b176ad3db66ec0654a9b3f94745646a51a
Hovooo <avakiandev@gmail.com>
2024-07-03 15:26:21 +0400

    Removed unneeded files

--- src/interfaces/vyper/ILiquidityGauge.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-// Liquidity gauge  interface
-interface ILiquidityGauge {
-    function deposit(uint256 amount, address user) external;
-
-    function initialize(address _receiptToken, address _minter, address _admin) external;
-}
