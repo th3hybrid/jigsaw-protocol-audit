c49b00b71a65fa82156b0093ab43dc6856fda94f
Hovooo <avakiandev@gmail.com>
2025-01-15 13:08:06 +0400

    Consolidate all deployment tests into a single file

--- test/scripts/DeployAll.t.sol
+++ test/scripts/DeployAll.t.sol
@@ -3,9 +3,29 @@ pragma solidity ^0.8.20;
 
 import "../fixtures/ScriptTestsFixture.t.sol";
 
-contract DeployManagersTest is Test, ScriptTestsFixture {
+contract DeployAll is Test, ScriptTestsFixture {
     function setUp() public {
         init();
+    }
+
+    function test_deploy_manager() public view {
+        // Perform checks on the Manager Contract
+        assertEq(manager.owner(), INITIAL_OWNER, "Initial owner in Manager is wrong");
+        assertEq(manager.USDC(), USDC, "USDC address in Manager is wrong");
+        assertEq(manager.WETH(), WETH, "WETH address in Manager is wrong");
+        assertEq(address(manager.jUsdOracle()), JUSD_Oracle, "JUSD_Oracle address in Manager is wrong");
+        assertEq(bytes32(manager.oracleData()), bytes32(""), "JUSD_OracleData in Manager is wrong");
+
+        // Perform checks on the ManagerContainer Contract
+        assertEq(managerContainer.owner(), INITIAL_OWNER, "Initial owner in ManagerContainer is wrong");
+        assertEq(managerContainer.manager(), address(manager), "Manager address in ManagerContainer is wrong");
+    }
+
+    function test_deploy_jUSD() public view {
+        // Perform checks on the JUSD Contract
+        assertEq(jUSD.owner(), INITIAL_OWNER, "Initial owner in jUSD is wrong");
+        assertEq(address(jUSD.managerContainer()), address(managerContainer), "ManagerContainer in jUSD is wrong");
+        assertEq(jUSD.decimals(), 18, "Decimals in jUSD is wrong");
     }
 
     function test_deploy_managers() public {
@@ -55,10 +75,10 @@ contract DeployManagersTest is Test, ScriptTestsFixture {
         // Imitate multisig calls
         vm.startPrank(INITIAL_OWNER, INITIAL_OWNER);
         manager.setHoldingManager(address(holdingManager));
-        manager.setHoldingManager(address(liquidationManager));
-        manager.setHoldingManager(address(stablesManager));
-        manager.setHoldingManager(address(strategyManager));
-        manager.setHoldingManager(address(swapManager));
+        manager.setLiquidationManager(address(liquidationManager));
+        manager.setStablecoinManager(address(stablesManager));
+        manager.setStrategyManager(address(strategyManager));
+        manager.setSwapManager(address(swapManager));
         vm.stopPrank();
 
         assertEq(manager.holdingManager(), address(holdingManager), "HoldingManager in Manager is wrong");
@@ -66,5 +86,34 @@ contract DeployManagersTest is Test, ScriptTestsFixture {
         assertEq(manager.stablesManager(), address(stablesManager), "StablesManager in Manager is wrong");
         assertEq(manager.strategyManager(), address(strategyManager), "StrategyManager in Manager is wrong");
         assertEq(manager.swapManager(), address(swapManager), "SwapManager in Manager is wrong");
+    }
+
+    function test_deploy_registries() public view {
+        for (uint256 i = 0; i < registries.length; i += 1) {
+            SharesRegistry registry = SharesRegistry(registries[i]);
+
+            // Perform checks on the ShareRegistry Contracts
+            assertEq(registry.owner(), INITIAL_OWNER, "INITIAL_OWNER in ShareRegistry is wrong");
+            assertEq(
+                address(registry.managerContainer()),
+                address(managerContainer),
+                "ManagerContainer in ShareRegistry is wrong"
+            );
+
+            // Perform checks on the StablesManager Contract
+            (bool active, address _registry) = stablesManager.shareRegistryInfo(registry.token());
+            assertEq(active, true, "Active flag in StablesManager is wrong");
+            assertEq(_registry, address(registry), "Registry address in StablesManager is wrong");
+        }
+    }
+
+    function test_deploy_receiptToken() public view {
+        // Perform checks on the ReceiptTokenFactory Contract
+        assertEq(receiptTokenFactory.owner(), INITIAL_OWNER, "INITIAL_OWNER in ReceiptTokenFactory is wrong");
+        assertEq(
+            receiptTokenFactory.referenceImplementation(),
+            address(receiptToken),
+            "ReferenceImplementation in ReceiptTokenFactory is wrong"
+        );
     }
 }
