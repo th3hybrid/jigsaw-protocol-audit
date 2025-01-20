225df68b4476a992212eb467c38dbb4440860504
Hovooo <avakiandev@gmail.com>
2025-01-20 14:36:45 +0400

    Updated Oracle interface

--- src/interfaces/oracle/IOracle.sol
+++ src/interfaces/oracle/IOracle.sol
@@ -3,27 +3,21 @@ pragma solidity ^0.8.20;
 
 interface IOracle {
     /**
-     * @notice Get the latest exchange rate.
-     *
-     * @dev Ensure it has 18 decimals for correct computations.
-     *
-     * @param data Usually abi encoded, implementation specific data that contains information and arguments to & about
-     * the oracle. For example: (string memory collateralSymbol, string memory assetSymbol, uint256 division) =
-     * abi.decode(data, (string, string, uint256));
-     *
-     * @return success If no valid (recent) rate is available, returns false else true.
-     * @return rate The rate of the requested asset / pair / pool.
+     * @notice Returns a human readable (short) name about this oracle.
+     * @return (string) A human readable symbol name about this oracle.
      */
-    function get(
-        bytes calldata data
-    ) external returns (bool success, uint256 rate);
+    function symbol() external view returns (string memory);
+
+    /**
+     * @notice Returns a human readable name about this oracle.
+     * @return (string) A human readable name about this oracle.
+     */
+    function name() external view returns (string memory);
 
     /**
      * @notice Check the last exchange rate without any state changes.
      *
-     * @param data Usually abi encoded, implementation specific data that contains information and arguments to & about
-     * the oracle. For example: (string memory collateralSymbol, string memory assetSymbol, uint256 division) =
-     * abi.decode(data, (string, string, uint256));
+     * @param data Implementation specific data that contains information and arguments to & about the oracle.
      *
      * @return success If no valid (recent) rate is available, returns false else true.
      * @return rate The rate of the requested asset / pair / pool.
@@ -31,44 +25,4 @@ interface IOracle {
     function peek(
         bytes calldata data
     ) external view returns (bool success, uint256 rate);
-
-    /**
-     * @notice Check the current spot exchange rate without any state changes. For oracles like TWAP this will be
-     * different from peek().
-     *
-     * @param data Usually abi encoded, implementation specific data that contains information and arguments to & about
-     * the oracle. For example: (string memory collateralSymbol, string memory assetSymbol, uint256 division) =
-     * abi.decode(data, (string, string, uint256));
-     *
-     * @return rate The rate of the requested asset / pair / pool.
-     */
-    function peekSpot(
-        bytes calldata data
-    ) external view returns (uint256 rate);
-
-    /**
-     * @notice Returns a human readable (short) name about this oracle.
-     *
-     * @param data Usually abi encoded, implementation specific data that contains information and arguments to & about
-     * the oracle. For example: (string memory collateralSymbol, string memory assetSymbol, uint256 division) =
-     * abi.decode(data, (string, string, uint256));
-     *
-     * @return (string) A human readable symbol name about this oracle.
-     */
-    function symbol(
-        bytes calldata data
-    ) external view returns (string memory);
-
-    /**
-     * @notice Returns a human readable name about this oracle.
-     *
-     * @param data Usually abi encoded, implementation specific data that contains information and arguments to & about
-     * the oracle. For example: (string memory collateralSymbol, string memory assetSymbol, uint256 division) =
-     * abi.decode(data, (string, string, uint256));
-     *
-     * @return (string) A human readable name about this oracle.
-     */
-    function name(
-        bytes calldata data
-    ) external view returns (string memory);
 }
