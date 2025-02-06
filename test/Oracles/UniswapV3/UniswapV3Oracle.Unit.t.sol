add0ee62b31f4640da7881f92396f366b627336e
Hovooo <avakiandev@gmail.com>
2025-02-06 16:09:23 +0530

    Updated pragma version

--- test/Oracles/UniswapV3/UniswapV3Oracle.Unit.t.sol
+++ test/Oracles/UniswapV3/UniswapV3Oracle.Unit.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.25;
+pragma solidity ^0.8.13;
 
 import { Test } from "forge-std/Test.sol";
 import { console } from "forge-std/console.sol";
