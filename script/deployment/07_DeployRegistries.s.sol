7262f9b554db0b67c3cde710c3f5959db8c87d2b
Hovooo <avakiandev@gmail.com>
2025-02-17 18:21:28 +0530

    Configured Registries' deployment script

--- script/deployment/07_DeployRegistries.s.sol
+++ script/deployment/07_DeployRegistries.s.sol
@@ -23,6 +23,7 @@ contract DeployRegistries is Script, Base {
     using StdJson for string;
 
     struct RegistryConfig {
+        string symbol;
         address token;
         uint256 collateralizationRate;
         bytes oracleData;
@@ -45,11 +46,6 @@ contract DeployRegistries is Script, Base {
     address internal MANAGER_CONTAINER = deployments.readAddress(".MANAGER_CONTAINER");
     address internal STABLES_MANAGER = deployments.readAddress(".STABLES_MANAGER");
     address internal PYTH_ORACLE_FACTORY = deployments.readAddress(".PYTH_ORACLE_FACTORY");
-
-    // Store configuration for each SharesRegistry
-    address internal USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-    uint256 internal USDC_CR = 50_000;
-    bytes32 internal USDC_PYTH_ID = 0xeaa020c61cc479712813461ce153894a96a6c00b21ed0cfc2798d1f9a9e9c94a;
 
     // Common configs for oracle
     bytes internal COMMON_ORACLE_DATA = bytes("");
@@ -102,9 +98,153 @@ contract DeployRegistries is Script, Base {
         // Add configs for desired collaterals' registries
         registryConfigs.push(
             RegistryConfig({
-                token: USDC,
-                collateralizationRate: USDC_CR,
-                pythId: USDC_PYTH_ID,
+                symbol: "USDC",
+                token: 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48,
+                collateralizationRate: 90_000,
+                pythId: 0xeaa020c61cc479712813461ce153894a96a6c00b21ed0cfc2798d1f9a9e9c94a,
+                oracleData: COMMON_ORACLE_DATA,
+                age: COMMON_ORACLE_AGE
+            })
+        );
+
+        registryConfigs.push(
+            RegistryConfig({
+                symbol: "USDT",
+                token: 0xdAC17F958D2ee523a2206206994597C13D831ec7,
+                collateralizationRate: 90_000,
+                pythId: 0x2b89b9dc8fdf9f34709a5b106b472f0f39bb6ca9ce04b0fd7f2e971688e2e53b,
+                oracleData: COMMON_ORACLE_DATA,
+                age: COMMON_ORACLE_AGE
+            })
+        );
+
+        registryConfigs.push(
+            RegistryConfig({
+                symbol: "DAI",
+                token: 0x6B175474E89094C44Da98b954EedeAC495271d0F,
+                collateralizationRate: 90_000,
+                pythId: 0xb0948a5e5313200c632b51bb5ca32f6de0d36e9950a942d19751e833f70dabfd,
+                oracleData: COMMON_ORACLE_DATA,
+                age: COMMON_ORACLE_AGE
+            })
+        );
+
+        registryConfigs.push(
+            RegistryConfig({
+                symbol: "sUSDe",
+                token: 0x9D39A5DE30e57443BfF2A8307A4256c8797A3497,
+                collateralizationRate: 90_000,
+                pythId: 0xca3ba9a619a4b3755c10ac7d5e760275aa95e9823d38a84fedd416856cdba37c,
+                oracleData: COMMON_ORACLE_DATA,
+                age: COMMON_ORACLE_AGE
+            })
+        );
+
+        registryConfigs.push(
+            RegistryConfig({
+                symbol: "USD0++",
+                token: 0x35D8949372D46B7a3D5A56006AE77B215fc69bC0,
+                collateralizationRate: 90_000,
+                pythId: 0xf9c96a45784d0ce4390825a43a313149da787e6a6c66076f3a3f83e92501baeb,
+                oracleData: COMMON_ORACLE_DATA,
+                age: COMMON_ORACLE_AGE
+            })
+        );
+
+        registryConfigs.push(
+            RegistryConfig({
+                symbol: "wBTC",
+                token: 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599,
+                collateralizationRate: 80_000,
+                pythId: 0xc9d8b075a5c69303365ae23633d4e085199bf5c520a3b90fed1322a0342ffc33,
+                oracleData: COMMON_ORACLE_DATA,
+                age: COMMON_ORACLE_AGE
+            })
+        );
+
+        registryConfigs.push(
+            RegistryConfig({
+                symbol: "wETH",
+                token: 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2,
+                collateralizationRate: 80_000,
+                pythId: 0x9d4294bbcd1174d6f2003ec365831e64cc31d9f6f15a2b85399db8d5000960f6,
+                oracleData: COMMON_ORACLE_DATA,
+                age: COMMON_ORACLE_AGE
+            })
+        );
+
+        registryConfigs.push(
+            RegistryConfig({
+                symbol: "wstETH",
+                token: 0x7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0,
+                collateralizationRate: 80_000,
+                pythId: 0x6df640f3b8963d8f8358f791f352b8364513f6ab1cca5ed3f1f7b5448980e784,
+                oracleData: COMMON_ORACLE_DATA,
+                age: COMMON_ORACLE_AGE
+            })
+        );
+
+        registryConfigs.push(
+            RegistryConfig({
+                symbol: "rswETH",
+                token: 0xFAe103DC9cf190eD75350761e95403b7b8aFa6c0,
+                collateralizationRate: 75_000,
+                pythId: 0x17e349391a4d8362706ec4126c2fa42047601cb71c1063e38ca305fab9b0ec4d,
+                oracleData: COMMON_ORACLE_DATA,
+                age: COMMON_ORACLE_AGE
+            })
+        );
+
+        registryConfigs.push(
+            RegistryConfig({
+                symbol: "pufETH",
+                token: 0xD9A442856C234a39a81a089C06451EBAa4306a72,
+                collateralizationRate: 75_000,
+                pythId: 0xe5801530292c348f322b7b4a48c1c0d59ab629846cce1c816fc27aee2054b560,
+                oracleData: COMMON_ORACLE_DATA,
+                age: COMMON_ORACLE_AGE
+            })
+        );
+
+        registryConfigs.push(
+            RegistryConfig({
+                symbol: "weETH",
+                token: 0xCd5fE23C85820F7B72D0926FC9b05b43E359b7ee,
+                collateralizationRate: 75_000,
+                pythId: 0x9ee4e7c60b940440a261eb54b6d8149c23b580ed7da3139f7f08f4ea29dad395,
+                oracleData: COMMON_ORACLE_DATA,
+                age: COMMON_ORACLE_AGE
+            })
+        );
+
+        registryConfigs.push(
+            RegistryConfig({
+                symbol: "ezETH",
+                token: 0xbf5495Efe5DB9ce00f80364C8B423567e58d2110,
+                collateralizationRate: 75_000,
+                pythId: 0x06c217a791f5c4f988b36629af4cb88fad827b2485400a358f3b02886b54de92,
+                oracleData: COMMON_ORACLE_DATA,
+                age: COMMON_ORACLE_AGE
+            })
+        );
+
+        registryConfigs.push(
+            RegistryConfig({
+                symbol: "pxETH",
+                token: 0x04C154b66CB340F3Ae24111CC767e0184Ed00Cc6,
+                collateralizationRate: 75_000,
+                pythId: 0x834be8951394714988606b3a1ac299c48bd07d68e5abb02766bcf881fdc1e69c,
+                oracleData: COMMON_ORACLE_DATA,
+                age: COMMON_ORACLE_AGE
+            })
+        );
+
+        registryConfigs.push(
+            RegistryConfig({
+                symbol: "LBTC",
+                token: 0x8236a87084f8B84306f72007F36F2618A5634494,
+                collateralizationRate: 75_000,
+                pythId: 0x8f257aab6e7698bb92b15511915e593d6f8eae914452f781874754b03d0c612b,
                 oracleData: COMMON_ORACLE_DATA,
                 age: COMMON_ORACLE_AGE
             })
