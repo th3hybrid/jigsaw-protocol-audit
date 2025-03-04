1bd7a67000fedad0ee85891822ce4ff39d12424c
Hovooo <avakiandev@gmail.com>
2025-03-04 22:56:54 +0530

    Updated tests

--- test/Oracles/Pyth/PythOracle.Unit.t.sol
+++ test/Oracles/Pyth/PythOracle.Unit.t.sol
@@ -92,7 +92,7 @@ contract PythOracleUnitTest is Test {
         (bool success, uint256 rate) = pythOracle.peek("");
 
         vm.assertEq(success, true, "Peek failed");
-        vm.assertEq(rate, 3_295_633_182_430_000_000_000, "Rate is wrong");
+        vm.assertEq(rate, 3_286_215_397_790_000_000_000, "Rate is wrong");
     }
 
     function test_pyth_peek_when_NegativeOraclePrice() public {
@@ -109,7 +109,7 @@ contract PythOracleUnitTest is Test {
         _updateMockPythPrice(int64(-1), int32(-8));
 
         // Expect the next call to revert with the correct error
-        vm.expectRevert(IPythOracle.NegativeOraclePrice.selector);
+        vm.expectRevert(IPythOracle.InvalidOraclePrice.selector);
         pythOracle.peek("");
     }
 
@@ -149,6 +149,26 @@ contract PythOracleUnitTest is Test {
         pythOracle.peek("");
     }
 
+    function test_pyth_peek_when_notConfident() public {
+        pythOracle = PythOracle(
+            mockPythOracleFactory.createPythOracle({
+                _initialOwner: OWNER,
+                _underlying: WETH,
+                _priceId: PRICE_ID,
+                _age: AGE
+            })
+        );
+
+        // Set pyth price with a small expo
+        _updateMockPythPrice(int64(100_000_000), uint64(1_000_000), int32(-8));
+
+        // Expect the next call to revert with the correct error
+        (bool success, uint256 rate) = pythOracle.peek("");
+
+        vm.assertEq(success, true, "Peek failed");
+        vm.assertEq(rate, (100_000_000 - 1_000_000) * 10 ** (18 - 8), "Rate is wrong");
+    }
+
     function test_pyth_updateAge(
         uint256 _newAge
     ) public withRegularOracle {
@@ -173,6 +193,36 @@ contract PythOracleUnitTest is Test {
         vm.stopPrank();
     }
 
+    function test_pyth_updateConfidencePercentage(
+        uint256 _newConfidence
+    ) public withRegularOracle {
+        _newConfidence = bound(_newConfidence, 1, pythOracle.CONFIDENCE_PRECISION());
+
+        vm.expectRevert(abi.encodeWithSelector(OwnableUnauthorizedAccount.selector, address(this)));
+        pythOracle.updateConfidencePercentage(0);
+
+        vm.startPrank(OWNER, OWNER);
+        vm.expectRevert(IPythOracle.InvalidConfidencePercentage.selector);
+        pythOracle.updateConfidencePercentage(0);
+
+        uint256 oldConfidence = pythOracle.MIN_CONFIDENCE_PERCENTAGE();
+        vm.expectRevert(IPythOracle.InvalidConfidencePercentage.selector);
+        pythOracle.updateConfidencePercentage(oldConfidence);
+
+        vm.startPrank(OWNER, OWNER);
+        vm.expectRevert(IPythOracle.InvalidConfidencePercentage.selector);
+        pythOracle.updateConfidencePercentage(1e4 + 1);
+
+        vm.assume(_newConfidence != oldConfidence);
+
+        vm.expectEmit();
+        emit IPythOracle.ConfidencePercentageUpdated({ oldValue: oldConfidence, newValue: _newConfidence });
+        pythOracle.updateConfidencePercentage(_newConfidence);
+
+        vm.assertEq(pythOracle.MIN_CONFIDENCE_PERCENTAGE(), _newConfidence, "Confidence percentage wrong after update");
+        vm.stopPrank();
+    }
+
     function test_pyth_renounceOwnership() public withRegularOracle {
         vm.expectRevert(bytes("1000"));
         pythOracle.renounceOwnership();
@@ -185,6 +235,20 @@ contract PythOracleUnitTest is Test {
                 PRICE_ID,
                 PythStructs.Price(_price, uint64(1), _expo, vm.getBlockTimestamp()),
                 PythStructs.Price(_price, uint64(1), _expo, vm.getBlockTimestamp())
+            ),
+            0
+        );
+
+        mockPyth.updatePriceFeeds(priceUpdateData);
+    }
+
+    function _updateMockPythPrice(int64 _price, uint64 _confidence, int32 _expo) private {
+        bytes[] memory priceUpdateData = new bytes[](1);
+        priceUpdateData[0] = abi.encode(
+            PythStructs.PriceFeed(
+                PRICE_ID,
+                PythStructs.Price(_price, _confidence, _expo, vm.getBlockTimestamp()),
+                PythStructs.Price(_price, _confidence, _expo, vm.getBlockTimestamp())
             ),
             0
         );
