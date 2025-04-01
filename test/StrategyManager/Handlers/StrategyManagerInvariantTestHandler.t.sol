4bf7b5bf1db753c23f3ecdaab0243e65fd106c8f
Tigran Arakelyan <traqel@gmail.com>
2025-04-01 19:04:15 +0400

    add test

---
+++ test/StrategyManager/Handlers/StrategyManagerInvariantTestHandler.t.sol
@@ -0,0 +1,125 @@
+pragma solidity ^0.8.20;
+
+import {CommonBase} from "forge-std/Base.sol";
+import {StdCheats} from "forge-std/StdCheats.sol";
+import {StdUtils} from "forge-std/StdUtils.sol";
+import {stdMath} from "forge-std/stdMath.sol";
+
+import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import {IERC20, IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+
+import {HoldingManager} from "../../../src/HoldingManager.sol";
+import {SharesRegistry} from "../../../src/SharesRegistry.sol";
+import {StrategyManager} from "../../../src/StrategyManager.sol";
+import {StrategyWithRewardsYieldsMock} from "../../utils/mocks/StrategyWithRewardsYieldsMock.sol";
+
+contract StrategyManagerInvariantTestHandler is CommonBase, StdCheats, StdUtils {
+    using EnumerableSet for EnumerableSet.AddressSet;
+    using stdMath for int256;
+
+    HoldingManager internal holdingManager;
+    StrategyManager internal strategyManager;
+    SharesRegistry internal sharesRegistry;
+    StrategyWithRewardsYieldsMock internal strategy;
+
+    address internal collateral;
+    mapping(address => uint256) internal collateralDeposited;
+
+    mapping(address => address) internal userHolding;
+    address[] public USER_ADDRESSES = [
+        address(uint160(uint256(keccak256("user1")))),
+        address(uint160(uint256(keccak256("user2")))),
+        address(uint160(uint256(keccak256("user3")))),
+        address(uint160(uint256(keccak256("user4")))),
+        address(uint160(uint256(keccak256("user5"))))
+    ];
+
+    constructor(
+        StrategyWithRewardsYieldsMock _strategy,
+        HoldingManager _holdingManager,
+        StrategyManager _strategyManager,
+        SharesRegistry _sharesRegistry,
+        address _collateral
+    ) {
+        strategy = _strategy;
+        holdingManager = _holdingManager;
+        strategyManager = _strategyManager;
+        sharesRegistry = _sharesRegistry;
+        collateral = _collateral;
+        createUserHoldings();
+    }
+
+    function user_invest(uint256 _mintAmount, uint256 user_idx) external {
+        _mintAmount = bound(_mintAmount, 1, 100_000e18);
+        address user = pickUpUser(user_idx);
+
+        IERC20Metadata collateralContract = IERC20Metadata(collateral);
+
+        uint256 collateralAmount = _mintAmount * 2;
+
+        //get tokens for user
+        deal(collateral, user, collateralAmount);
+
+        // make deposit to the holding
+        vm.startPrank(user, user);
+        collateralContract.approve(address(holdingManager), collateralAmount);
+        holdingManager.deposit(collateral, collateralAmount);
+        strategyManager.invest(collateral, address(strategy), _mintAmount, 0, bytes(""));
+        vm.stopPrank();
+
+        collateralDeposited[user] += collateralAmount;
+    }
+
+    function user_claim_invested(uint256 _claimAmount, uint256 user_idx) external {
+        address user = pickUpUser(user_idx);
+
+        // make deposit to the holding
+        vm.startPrank(user, user);
+        (, uint256 shares) = strategy.recipients(userHolding[user]);
+        vm.assume(shares > 0);
+
+        _claimAmount = bound(_claimAmount, 1, shares);
+        strategyManager.claimInvestment(userHolding[user], collateral, address(strategy), _claimAmount, "");
+
+        vm.stopPrank();
+        collateralDeposited[user] -= _claimAmount;
+
+        if(strategy.yieldAmount() > 0) {
+            collateralDeposited[user] -= strategy.yieldAmount().abs();
+        }
+        else {
+            collateralDeposited[user] += strategy.yieldAmount().abs();
+        }
+    }
+
+    function set_strategy_yield(int256 _amount) external {
+        vm.assume(_amount.abs() < 1 && _amount < 1e20);
+        strategy.setYield(_amount);
+    }
+
+    function getTotalCollateral() external view returns (uint256 totalCollateral) {
+        for (uint256 i = 0; i < USER_ADDRESSES.length; i++) {
+            totalCollateral += collateralDeposited[USER_ADDRESSES[i]];
+        }
+    }
+
+    function getTotalCollateralFromRegistry() external view returns (uint256 totalCollateralFromRegistry) {
+        for (uint256 i = 0; i < USER_ADDRESSES.length; i++) {
+            totalCollateralFromRegistry += sharesRegistry.collateral(userHolding[USER_ADDRESSES[i]]);
+        }
+    }
+
+    function createUserHoldings() private {
+        for (uint256 i = 0; i < USER_ADDRESSES.length; i++) {
+            address user = USER_ADDRESSES[i];
+            vm.startPrank(user, user);
+            userHolding[user] = holdingManager.createHolding();
+            vm.stopPrank();
+        }
+    }
+
+    function pickUpUser(uint256 _user_idx) public view returns (address) {
+        _user_idx = _user_idx % USER_ADDRESSES.length;
+        return USER_ADDRESSES[_user_idx];
+    }
+}
---
+++ test/StrategyManager/StrategyManagerInvariantTest.t.sol
@@ -0,0 +1,53 @@
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+
+import {BasicContractsFixture} from "../fixtures/BasicContractsFixture.t.sol";
+import {StrategyWithRewardsYieldsMock} from "../utils/mocks/StrategyWithRewardsYieldsMock.sol";
+import {StrategyManagerInvariantTestHandler} from "./Handlers/StrategyManagerInvariantTestHandler.t.sol";
+
+/// @title StrategyManagerInvariantTest
+/// @author Hovooo (@hovooo)
+/// @notice This contract is designed to invariant test StrategyManager contract.
+/// @dev This invariant test allows both testing with and without reverts. To disable reverts, set the
+/// WITH_REVERTS variable to false.
+contract StrategyManagerInvariantTest is Test, BasicContractsFixture {
+
+    StrategyManagerInvariantTestHandler private handler;
+
+    function setUp() external {
+        init();
+
+        StrategyWithRewardsYieldsMock strategyWithPositiveYield = new StrategyWithRewardsYieldsMock(
+            address(manager),
+            address(usdc),
+            address(usdc),
+            address(0),
+            "AnotherMockWithYield",
+            "AMWY"
+        );
+
+        vm.prank(OWNER, OWNER);
+        strategyManager.addStrategy(address(strategyWithPositiveYield));
+
+        handler = new StrategyManagerInvariantTestHandler(
+            strategyWithPositiveYield,
+            holdingManager,
+            strategyManager,
+            sharesRegistry,
+            address(usdc)
+        );
+
+        targetContract(address(handler));
+    }
+
+    // Test that share registrie's deposited collateral amount is correct at all times
+    function invariant_stablesManager_totalCollateralInRegistry() public view {
+        vm.assertApproxEqRel(
+            handler.getTotalCollateralFromRegistry(),
+            handler.getTotalCollateral(),
+            1e18,
+            "Total collateral amount in registry is incorrect"
+        );
+    }
+}
