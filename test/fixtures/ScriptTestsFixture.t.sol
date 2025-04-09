0313e4d54d97bd560bd4f46e197c23fb8f025600
Hovooo <avakiandev@gmail.com>
2025-04-09 13:39:49 +0530

    Updated tests

--- test/fixtures/ScriptTestsFixture.t.sol
+++ test/fixtures/ScriptTestsFixture.t.sol
@@ -12,7 +12,7 @@ import { DeployManager } from "../../script/deployment/01_DeployManager.s.sol";
 import { DeployJUSD } from "../../script/deployment/03_DeployJUSD.s.sol";
 import { DeployManagers } from "../../script/deployment/04_DeployManagers.s.sol";
 import { DeployReceiptToken } from "../../script/deployment/05_DeployReceiptToken.s.sol";
-import { DeployPythOracleFactory } from "../../script/deployment/06_DeployPythOracleFactory.s.sol";
+import { DeployChronicleOracleFactory } from "../../script/deployment/06_DeployChronicleOracleFactory.s.sol";
 import { DeployRegistries } from "../../script/deployment/07_DeployRegistries.s.sol";
 import { DeployUniswapV3Oracle } from "../../script/deployment/08_DeployUniswapV3Oracle.s.sol";
 import { DeployMocks } from "../../script/mocks/00_DeployMocks.s.sol";
@@ -29,8 +29,8 @@ import { StablesManager } from "../../src/StablesManager.sol";
 import { StrategyManager } from "../../src/StrategyManager.sol";
 import { SwapManager } from "../../src/SwapManager.sol";
 
-import { PythOracle } from "../../src/oracles/pyth/PythOracle.sol";
-import { PythOracleFactory } from "../../src/oracles/pyth/PythOracleFactory.sol";
+import { ChronicleOracle } from "../../src/oracles/chronicle/ChronicleOracle.sol";
+import { ChronicleOracleFactory } from "../../src/oracles/chronicle/ChronicleOracleFactory.sol";
 
 import { UniswapV3Oracle } from "src/oracles/uniswap/UniswapV3Oracle.sol";
 
@@ -44,7 +44,6 @@ contract ScriptTestsFixture is Test {
     string internal commonConfigPath = "./deployment-config/00_CommonConfig.json";
     string internal managerConfigPath = "./deployment-config/01_ManagerConfig.json";
     string internal managersConfigPath = "./deployment-config/03_ManagersConfig.json";
-    string internal pythConfigPath = "./deployment-config/04_PythConfig.json";
     string internal uniswapV3OracleConfigPath = "./deployment-config/05_UniswapV3OracleConfig.json";
 
     address internal INITIAL_OWNER = vm.addr(vm.envUint("DEPLOYER_PRIVATE_KEY"));
@@ -54,8 +53,6 @@ contract ScriptTestsFixture is Test {
 
     address internal UNISWAP_FACTORY = 0x1F98431c8aD98523631AE4a59f267346ea31F984;
     address internal UNISWAP_SWAP_ROUTER = 0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45;
-
-    address internal PYTH = 0x4305FB66699C3B2702D4d05CF36551390A4c69C6;
 
     address internal USDT_USDC_POOL = 0x3416cF6C708Da44DB2624D63ea0AAef7113527C6; // pretend that this is jUSD/USDC pool
 
@@ -69,8 +66,8 @@ contract ScriptTestsFixture is Test {
     SwapManager internal swapManager;
     ReceiptToken internal receiptToken;
     ReceiptTokenFactory internal receiptTokenFactory;
-    PythOracle internal pythOracleImpl;
-    PythOracleFactory internal pythOracleFactory;
+    ChronicleOracle internal chronicleOracle;
+    ChronicleOracleFactory internal chronicleOracleFactory;
     UniswapV3Oracle internal jUsdUniswapV3Oracle;
 
     address[] internal registries;
@@ -92,7 +89,6 @@ contract ScriptTestsFixture is Test {
         Strings.toHexString(uint256(bytes32("")), 32).write(managerConfigPath, ".JUSD_OracleData");
         Strings.toHexString(uint160(UNISWAP_FACTORY), 20).write(managersConfigPath, ".UNISWAP_FACTORY");
         Strings.toHexString(uint160(UNISWAP_SWAP_ROUTER), 20).write(managersConfigPath, ".UNISWAP_SWAP_ROUTER");
-        Strings.toHexString(uint160(PYTH), 20).write(pythConfigPath, ".PYTH");
         Strings.toHexString(uint160(USDT_USDC_POOL), 20).write(uniswapV3OracleConfigPath, ".JUSD_USDC_UNISWAP_POOL");
         Strings.toHexString(uint160(USDC), 20).write(uniswapV3OracleConfigPath, ".USDC");
         Strings.toHexString(uint160(JUSD_Oracle), 20).write(uniswapV3OracleConfigPath, ".USDC_ORACLE");
@@ -109,9 +105,9 @@ contract ScriptTestsFixture is Test {
         DeployManagers deployManagersScript = new DeployManagers();
         (holdingManager, liquidationManager, stablesManager, strategyManager, swapManager) = deployManagersScript.run();
 
-        //Run PythOracleFactory deployment script
-        DeployPythOracleFactory deployPythOracleFactory = new DeployPythOracleFactory();
-        (pythOracleFactory, pythOracleImpl) = deployPythOracleFactory.run();
+        //Run ChronicleOracleFactory deployment script
+        DeployChronicleOracleFactory deployChronicleOracleFactory = new DeployChronicleOracleFactory();
+        (chronicleOracleFactory, chronicleOracle) = deployChronicleOracleFactory.run();
 
         //Run ReceiptToken deployment script
         DeployReceiptToken deployReceiptTokenScript = new DeployReceiptToken();
